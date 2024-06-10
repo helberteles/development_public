7a59de5a8a0034af125ef2476cf43bb861890142
HELBER PATRICK TELES <HELBER.TELES@ibm.com>
2024-06-09 21:39:56 -0300

    Upload code java

---
+++ AprendaJava7dias/dia1-projeto/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner idadeScanner = new Scanner(System.in);
+        System.out.println("Digite a sua idade: ");
+        byte idadeCliente = idadeScanner.nextByte();
+        System.out.println(idadeCliente);
+    }
+
+
+}
