0b668b1ef0731dc84242f9aeb2611fd64d3e6096
HELBER PATRICK TELES <HELBER.TELES@ibm.com>
2024-07-09 22:15:36 -0300

    Update learning Java in 7 days

---
+++ AprendaJava7dias/workspace/Dia4desafio1A.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Dia4desafio1A {
+    public static void main(String[] args) {
+        for (int i=0; i<= 100; i++)
+        if(i % 2 ==0)
+            System.out.println(i + " ");
+    }
+}
---
+++ AprendaJava7dias/workspace/Dia4desafio2B.java
@@ -0,0 +1,8 @@
+
+public class Dia4desafio2B {
+    public static void main(String[] args) {
+        String spaces = "Hello World";
+        for (int i = 0; i <= spaces.length() - 1; i++)
+            System.out.print(spaces.charAt(i)+" ");
+    }
+}
